e91c6c5fff6cd3cf8f7f5a87f916a66b4c5ab32b
daisuke oku <w.40141@gmail.com>
2018-06-04 22:59:00 +0900

    add 0030

--- Volume0/0030.c
+++ Volume0/0030.c
@@ -1,38 +1,36 @@
 #include <stdio.h>
 
 int n, s, count = 0,cur;
-int digits[10];
+int digits[10] = {};
+int dataSet[50] = {};
 
 void dfs(int cur){
 	if(cur == n){
 		int sum = 0;
-		for(int i = 0; i < n; i++){
-			sum += digits[i];
-		}
-		if(sum == s){
-			count++;
-			for (int i = 0; i < 10; i++) {
-				printf("%d ", digits[i]);
-			}
-			printf("\n");
-		}
+		for(int i = 0; i < n; i++) sum += digits[i];
+		if(sum == s) count++;
 		return;
 	}
-	// int flag = 0;
 	for (int i = cur; i < 10; i++) {
-		digits[cur] = i; //重複を避ける
+		if (digits[cur-1] < i) {
+			digits[cur] = i;
+		}
 		dfs(cur+1);
 	}
 }
 
 int main(void){
+	int num = 0;
 	while(scanf("%d %d",&n,&s)!= EOF){
 		if(n == 0 && s == 0){
+			for (int i = 0; i < num; i++) printf("%d\n", dataSet[i]);
 			break;
 		}
 		count = 0;
 		dfs(0);
-		printf("%d\n",count);
+		dataSet[num] = count;
+		num++;
+		for(int i = 0; i < 10; i++) digits[i] = 0;
 	}
 	return 0;
 }
