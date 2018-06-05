797c7b0053c16ee0c838b87ebc2a063e672fcc87
daisuke oku <w.40141@gmail.com>
2018-06-05 21:27:42 +0900

    0030.c dekita

--- Volume0/0030.c
+++ Volume0/0030.c
@@ -2,34 +2,39 @@
 
 int n, s, count = 0,cur;
 int digits[10] = {};
-int dataSet[50] = {};
 
 void dfs(int cur){
 	if(cur == n){
 		int sum = 0;
-		for(int i = 0; i < n; i++) sum += digits[i];
-		if(sum == s) count++;
+		for(int i = 0; i < n; i++) {
+			sum += digits[i];
+		}
+		if(sum == s) {
+			count++;
+		}
 		return;
 	}
-	for (int i = cur; i < 10; i++) {
-		if (digits[cur-1] < i) {
+	for (int i = 0; i < 10; i++) {
+		if (cur == 0) {
 			digits[cur] = i;
+			dfs(cur+1);
+		} else {
+			if (digits[cur-1] < i) {
+				digits[cur] = i;
+				dfs(cur+1);
+			}
 		}
-		dfs(cur+1);
 	}
 }
 
 int main(void){
-	int num = 0;
 	while(scanf("%d %d",&n,&s)!= EOF){
 		if(n == 0 && s == 0){
-			for (int i = 0; i < num; i++) printf("%d\n", dataSet[i]);
 			break;
 		}
 		count = 0;
 		dfs(0);
-		dataSet[num] = count;
-		num++;
+		printf("%d\n", count);
 		for(int i = 0; i < 10; i++) digits[i] = 0;
 	}
 	return 0;
